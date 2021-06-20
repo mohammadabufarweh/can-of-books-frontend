e330fef53c0fd142b5958813c538c7ce1a83d62b
Mohammadabufarweh <abufarweh34@gmail.com>
2021-06-21 00:04:31 +0300

    lab-11/majd/mohammad

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Header from './Header';
+import IsLoadingAndError from './IsLoadingAndError';
+import Footer from './Footer';
+import MyFavoriteBooks from './MyFavoriteBooks';
+import Profile from './Profile';
+import Login from './Login';
+import { withAuth0 } from "@auth0/auth0-react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+class App extends React.Component {
+
+  render() {
+    const { isAuthenticated } = this.props.auth0;
+    console.log('app', this.props)
+    return (
+      <>
+        <Router>
+          {/* <IsLoadingAndError> */}
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+              {isAuthenticated &&
+                <MyFavoriteBooks />}
+              {!isAuthenticated &&
+                <Login />}
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Route exact path="/profile">
+
+              <Profile />
+            </Route>
+
+          </Switch>
+          <Footer class ='footerCss' />
+          {/* </IsLoadingAndError> */}
+        </Router>
+      </>
+    )
+  }
+}
+
+export default withAuth0(App)
