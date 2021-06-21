442adf5d233d625f14a4ef0644f2432b2ff0801f
Mohammadabufarweh <abufarweh34@gmail.com>
2021-06-22 00:09:29 +0300

    lab-12

--- src/MyFavoriteBooks.js
+++ src/MyFavoriteBooks.js
@@ -2,16 +2,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './myFavoriteBooks.css';
+import BestBook from './components/BestBook';
 
 class MyFavoriteBooks extends React.Component {
   render() {
-    return(
+    return (
+      <>
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
       </Jumbotron>
+      <BestBook/>
+      </>
     )
   }
 }
